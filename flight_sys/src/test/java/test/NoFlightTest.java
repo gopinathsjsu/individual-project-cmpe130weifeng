769d167cbb0fdd8ef060db520f8a99e8dff61951
Ray Ma <32551600+cmpe130weifeng@users.noreply.github.com>
2022-05-05 23:40:20 -0700

    Add files via upload

--- flight_sys/src/test/java/test/NoFlightTest.java
+++ flight_sys/src/test/java/test/NoFlightTest.java
@@ -1,1 +1,21 @@
+package test;
 
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static test.NoFlight.getFlight;
+
+class NoFlightTest {
+    // BeforeAll must be static
+    @BeforeAll
+    public static void setupAll() {
+        System.out.println("This test is built for testing null object pattern.");
+    }
+
+    @Test
+    public void shouldCreateFlight() {
+        assertNull(getFlight("AK470"));
+        assertNotNull(getFlight("SJ456"));
+    }
+}
